cfaa8fd3d56937ab124d3954de740781527b96a6
KKShaminda <shamindakaveesha72@gmail.com>
2025-10-31 17:13:38 +0530

    Create Nav bar

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,27 @@
+import './NavBar.css';
+import logo from '../../assets/Lovelytech-black.png';
+
+export default function Navbar() {
+  return (
+    <header className="lt-navbar">
+      <div className="lt-navbar__inner">
+        <div className="lt-brand">
+          <img src={logo} alt="Lovely Tech" className="lt-brand__logo" />
+          <span className="lt-brand__name">Lovely Tech</span>
+        </div>
+
+        <nav className="lt-nav" aria-label="Main navigation">
+          <a className="lt-nav__link" href="/">Home</a>
+          <a className="lt-nav__link" href="/about">About Us</a>
+          <a className="lt-nav__link" href="/products">Products</a>
+          <a className="lt-nav__link" href="/services">Services</a>
+          <a className="lt-nav__link" href="/contact">Contact Us</a>
+        </nav>
+
+        <div className="lt-actions">
+          <button className="lt-btn lt-btn--outline">Sign In</button>
+        </div>
+      </div>
+    </header>
+  );
+}
