849a7c5ef3c6d75af02c72f963e15803f8d56591
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2025-11-07 23:56:30 +0530

    create login

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginSignup from './components/LoginSignup/LoginSignup';
+
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +27,15 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <NavBar />
+      <Router>
+        <Routes>
+          <Route path='/login' element={<LoginSignup/>} />
+        </Routes>
+      </Router>
+      <Footer />
+>>>>>>> Stashed changes
     </div>
   );
 }
