1672464b88ff49c63d1c5e827a23785e24e4b9ec
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2026-07-03 23:11:09 +0530

    Update Sign In ✏️

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
+import { SigninPage } from './pages/login/SigninPage'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
+
+          {/* Home Page */}
           <Route path="/" element={<p>Home Page</p>} />
+
+          {/* Authentication Routes */}
+          <Route path="/login" element={<SigninPage />} />
+        
         </Routes>
       </Router>
     </>
