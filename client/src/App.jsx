33482b17bb7ac2e67786d78c058a2fea0c7ddc30
MSCPerera <164726262+MSCPerera@users.noreply.github.com>
2026-07-04 20:46:54 +0530

    Create Sign-up Part ≡ƒÜÇ

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { SigninPage } from './pages/login/SigninPage'
+import { SignupPage } from './pages/login/SignupPage'
 import { UserDashboard } from './pages/dashboard/UserDashboard'
 import { AdminDashboard } from './pages/dashboard/AdminDashboard'
 import { ReceptionistDashboard } from './pages/dashboard/ReceptionistDashboard'
@@ -16,6 +17,7 @@ function App() {
 
           {/* Authentication Routes */}
           <Route path="/login" element={<SigninPage />} />
+          <Route path="/signup" element={<SignupPage />} />
 
           {/* Role-based Dashboard Routes */}
           <Route path="/user/dashboard" element={<UserDashboard />} />
